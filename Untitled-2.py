bb698d22e045134434997dfa1deddbc9c30ab560
Madhanprakash.S <116624433+MadhanPrakash18@users.noreply.github.com>
2024-08-07 18:26:07 +0530

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,17 @@
+
+int  = input ( print("enter the first number "))
+int y = input (print ("enter the second number"))
+ int z= input  (print("enter the operation to perform"))
+
+
+if add:
+    print(x+y)
+    
+elif sub:
+    print(x-y)
+    
+elif multi:
+    print(x*y)
+    
+elif div:
+    print(x/y)
